e201094ee1438699534f61df52ff0ad850b962b9
Pradhumn Choudhary <pradhumnchoudhary96@gmail.com>
2022-10-05 17:12:33 +0530

    Add total_price in order

--- app/models/order.rb
+++ app/models/order.rb
@@ -1,4 +1,12 @@
 class Order < ApplicationRecord
   belongs_to :user
   has_many :order_items, :dependent => :destroy
+
+  def sub_total
+    sum = 0
+    order_items.each do |order_item|
+      sum += order_item.total_price
+    end
+    sum
+  end
 end
--- app/models/order_item.rb
+++ app/models/order_item.rb
@@ -1,4 +1,17 @@
 class OrderItem < ApplicationRecord
   belongs_to :order
   belongs_to :product
+
+  def total_price
+    if valid_quantity_and_price?
+      quantity.to_s.to_d * product.price.to_s.to_d
+    else
+      0.0
+    end
+  end
+  
+  def valid_quantity_and_price?
+    !quantity.to_s.strip.empty? && !product.price.to_s.strip.empty?
+  end
+
 end
