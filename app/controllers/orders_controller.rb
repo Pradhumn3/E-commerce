dfdf5f52a336074b6f07343e0baa3e0b31137227
Pradhumn3 <pradhumnchoudhary96@gmail.com>
2023-06-01 15:14:41 +0530

    Adding wicked Pdf for PDF

--- app/controllers/orders_controller.rb
+++ app/controllers/orders_controller.rb
@@ -1,6 +1,21 @@
 class OrdersController < ApplicationController
   
   def index
-    @orders = current_user.orders.paginate(:page => params[:page], :per_page => 2)  
+    @orders = current_user.orders.order('created_at DESC').paginate(:page => params[:page], :per_page => 2)
+    
+  end
+
+  def download_invoice
+    @order = Order.find_by_id(params[:id])
+    respond_to do |format|
+      format.html
+      format.pdf do
+        render pdf: "order_invoice_#{@order.id}",
+               template: "orders/download_invoice.html.erb",
+               disposition: :inline,
+              #  disposition: :attachment,
+               layout: "pdf.html"
+      end
+    end
   end
 end
